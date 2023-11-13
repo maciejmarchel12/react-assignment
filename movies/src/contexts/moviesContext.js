97264b9f4e5af2cbcddceb50924ffeaa8fa1adf5
Maciej Marchel <maciejmarchel14@gmail.com>
2023-11-13 19:54:55 +0000

    Initial Commit

---
+++ movies/src/contexts/moviesContext.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+export const MoviesContext = React.createContext(null);
+
+const MoviesContextProvider = (props) => {
+  const [favorites, setFavorites] = useState( [] )
+
+  const [myReviews, setMyReviews] = useState( {} ) 
+
+  const [mustWatch, setMustWatch] = useState( [] )
+
+  const addToMustWatch = (movie) => {
+    let newMustWatch = [];
+    if (!mustWatch.includes(movie.id)){
+      newMustWatch = [...mustWatch, movie.id];
+    }
+    else{
+      newMustWatch = [...mustWatch];
+    }
+    setMustWatch(newMustWatch)
+    console.log("Successfully added to must watch!")
+  }
+
+  const addToFavorites = (movie) => {
+    let newFavorites = [];
+    if (!favorites.includes(movie.id)){
+      newFavorites = [...favorites, movie.id];
+    }
+    else{
+      newFavorites = [...favorites];
+    }
+    setFavorites(newFavorites)
+  };
+  
+  // We will use this function in a later section
+  const removeFromFavorites = (movie) => {
+    setFavorites( favorites.filter(
+      (mId) => mId !== movie.id
+    ) )
+  };
+
+  const addReview = (movie, review) => {
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
+  //console.log(myReviews);
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        favorites,
+        addToFavorites,
+        removeFromFavorites,
+        addReview,
+        mustWatch,
+        addToMustWatch,
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
---
+++ movies/src/pages/upcoming.js
@@ -0,0 +1,73 @@
+import React from "react";
+import PageTemplate from '../components/templateMovieListPage'
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import { getUpcoming } from "../api/tmdb-api";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import AddToMustWatchIcon from "../components/cardIcons/addToMustWatch";
+
+const Upcoming = (props) => {
+
+  const { data, error, isLoading, isError } = useQuery('upcoming',getUpcoming);
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }
+
+  const movies = data.results
+
+  const mustWatch = movies.filter(m => m.mustWatch)
+  localStorage.setItem('favourites', JSON.stringify(mustWatch))
+  const addToMustWatch = (movieId) => true
+
+/*   const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favourites', JSON.stringify(favorites))
+  const addToFavorites = (movieId) => true */
+
+  return (
+    <PageTemplate
+      title='Discover Movies'
+      movies={movies}
+      action={(movie) => {
+        return <AddToMustWatchIcon movie={movie} />
+      }}
+    />
+  );
+};
+export default Upcoming;
+
+// IMPORTANT COMMENT: already refactored to included caching in exercise 1
+
+/* const Upcoming = (props) => {
+  
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
+  useEffect(() => {
+    getUpcoming().then(movies => {
+      setMovies(movies);
+    });
+  }, []);
+
+  return (
+    <PageTemplate
+      title='Discover Movies'
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
+    />
+  );
+}; */
