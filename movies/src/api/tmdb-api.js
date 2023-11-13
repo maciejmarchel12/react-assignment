3979df2668fecc1995c471aac3b4b924a0eb12f9
Maciej Marchel <maciejmarchel14@gmail.com>
2023-11-13 21:41:45 +0000

    Tried adding a latest page but its bugged, might replace it

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -87,3 +87,21 @@ export const getMovies = () => {
         throw error
       });
   }
+
+  //LATEST STATIC ENDPOINT API
+  export const getLatest = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/latest?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    )
+    .then((response) => {
+      if (!response.ok) {
+        return response.json().then((errorData) => {
+          throw new Error(errorData.message || 'Failed to fetch latest movies');
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+  };
--- movies/src/index.js
+++ movies/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import Upcoming from "./pages/upcoming";
+import Latest from "./pages/latest";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
@@ -29,6 +30,7 @@ const App = () => {
         <SiteHeader />
         <MoviesContextProvider>
         <Routes>
+        <Route path="/movies/latest" element={<Latest />} />
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/upcoming" element={ <Upcoming /> } />
         <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
